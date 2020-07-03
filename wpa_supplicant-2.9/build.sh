16f26036be3087079a6782eef7594d55bd539775
w00371129 <w00371129@notesmail.huawei.com>
2020-07-03 19:24:11 +0800

    TicketNo:AR000E0MI0 Description:trim wpa and hostapd Team:OTHERS Feature or Bugfix:Feature Binary Source:No PrivateCode(Yes/No):Yes
    Change-Id: Ie24b1570abb635706dd2fd9e7def1e9187615458
    Reviewed-on: http://mgit-tm.rnd.huawei.com/9570392
    Tested-by: public jenkins <public_jenkins@notesmail.huawei.com>
    Reviewed-by: yangguangzhao 00471801 <yangguangzhao1@huawei.com>
    Reviewed-by: libing 00373357 <libing23@huawei.com>

--- wpa_supplicant-2.9/build.sh
+++ wpa_supplicant-2.9/build.sh
@@ -5,29 +5,10 @@ OUT_DIR=$1
 ROOT_DIR=$(dirname "$0")
 BIN_DIR=$OUT_DIR/../bin/usr
 
-if [ -d $ROOT_DIR/os_dep/lib ];then
-    rm -rf $ROOT_DIR/os_dep/lib
-fi
-
-mkdir -p $ROOT_DIR/os_dep/lib/
-if [ -d $ROOT_DIR/os_dep/include ];then
-rm -rf $ROOT_DIR/os_dep/include
-fi
-
-cp $OUT_DIR/../libmessage_engine.* $ROOT_DIR/os_dep/lib/
-cp $OUT_DIR/../libhdf_osal.* $ROOT_DIR/os_dep/lib/
-cp $OUT_DIR/libsec.* $ROOT_DIR/os_dep/lib/
-
-echo "Trying to list $ROOT_DIR/os_dep/lib"
-if [ -f $ROOT_DIR/os_dep/lib/libmessage_engine.so ];then
-echo "libmessage_engine.so exists."
-fi
-ls -alh $ROOT_DIR/os_dep/lib/
-
 make -C $ROOT_DIR/wpa_supplicant/ clean
-make -C $ROOT_DIR/wpa_supplicant/ -j
+make DEPDIR=$OUT_DIR -C $ROOT_DIR/wpa_supplicant/ -j
 make -C $ROOT_DIR/hostapd/ clean
-make -C $ROOT_DIR/hostapd/ -j
+make DEPDIR=$OUT_DIR -C $ROOT_DIR/hostapd/ -j
 $ROOT_DIR/../../../prebuilts/gcc/linux-x86/arm/arm-linux-harmonyeabi-gcc/bin/arm-linux-harmonyeabi-strip $ROOT_DIR/wpa_supplicant/wpa_supplicant
 $ROOT_DIR/../../../prebuilts/gcc/linux-x86/arm/arm-linux-harmonyeabi-gcc/bin/arm-linux-harmonyeabi-strip $ROOT_DIR/hostapd/hostapd
 if [ ! -d $BIN_DIR ]; then
@@ -35,6 +16,3 @@ if [ ! -d $BIN_DIR ]; then
 fi
 cp $ROOT_DIR/wpa_supplicant/wpa_supplicant $BIN_DIR
 cp $ROOT_DIR/hostapd/hostapd $BIN_DIR
-
-rm -rf $ROOT_DIR/os_dep/lib
-
