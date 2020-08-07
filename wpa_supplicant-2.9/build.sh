3bfc07a5f0a1d1cbb7d9385a59e5478db1aed072
j00421640 <j00421640@notesmail.huawei.com>
2020-08-07 17:05:19 +0800

    TicketNo:AR000EMQH7 Description:wpa support ndk compile Team:OTHERS Feature or Bugfix:Bugfix Binary Source:No PrivateCode(Yes/No):No
    Change-Id: I2d48453282ef7e481109efe2451b7c1572a3c6b7
    Reviewed-on: http://mgit-tm.rnd.huawei.com/10196655
    Tested-by: public jenkins <public_jenkins@notesmail.huawei.com>
    Reviewed-by: yangguangzhao 00471801 <yangguangzhao1@huawei.com>
    Reviewed-by: huhuifeng 00359022 <huhuifeng@huawei.com>
    Reviewed-by: zhaopeng 00380337 <zhaopeng22@huawei.com>

--- wpa_supplicant-2.9/build.sh
+++ wpa_supplicant-2.9/build.sh
@@ -5,29 +5,29 @@ OUT_DIR=$1
 ROOT_DIR=$(dirname "$0")
 BIN_DIR=$OUT_DIR/bin/usr
 
-if [ -d "$ROOT_DIR/build_so/objs" ]; then
-rm -rf $ROOT_DIR/build_so/objs
+if [ -d "$ROOT_DIR/build/objs" ]; then
+rm -rf $ROOT_DIR/build/objs
 fi
-mkdir -p $ROOT_DIR/build_so/objs
+mkdir -p $ROOT_DIR/build/objs
 
 make -C $ROOT_DIR/wpa_supplicant/ clean
 make DEPDIR=$OUT_DIR -C $ROOT_DIR/wpa_supplicant/ -j
 make -C $ROOT_DIR/hostapd/ clean
 make DEPDIR=$OUT_DIR -C $ROOT_DIR/hostapd/ -j
 
-make -C $ROOT_DIR/build_so/ clean
-make DEPDIR=$OUT_DIR -C $ROOT_DIR/build_so/
+make -C $ROOT_DIR/build/ clean
+make DEPDIR=$OUT_DIR -C $ROOT_DIR/build/
 
-$ROOT_DIR/../../../prebuilts/gcc/linux-x86/arm/arm-linux-harmonyeabi-gcc/bin/arm-linux-harmonyeabi-strip $ROOT_DIR/build_so/libwpa.so
-cp $ROOT_DIR/build_so/libwpa.so $OUT_DIR
-rm -rf $ROOT_DIR/build_so/objs
+$ROOT_DIR/../../../prebuilts/gcc/linux-x86/arm/arm-linux-harmonyeabi-gcc/bin/arm-linux-harmonyeabi-strip $ROOT_DIR/build/libwpa.so
+cp $ROOT_DIR/build/libwpa.so $OUT_DIR
+rm -rf $ROOT_DIR/build/objs
 
+make DEPDIR=$OUT_DIR -C $ROOT_DIR/wpa_supplicant/ libwpa_client.so -j
+$ROOT_DIR/../../../prebuilts/gcc/linux-x86/arm/arm-linux-harmonyeabi-gcc/bin/arm-linux-harmonyeabi-strip $ROOT_DIR/wpa_supplicant/libwpa_client.so
+cp $ROOT_DIR/wpa_supplicant/libwpa_client.so $OUT_DIR
 
-#$ROOT_DIR/../../../prebuilts/gcc/linux-x86/arm/arm-linux-harmonyeabi-gcc/bin/arm-linux-harmonyeabi-strip $ROOT_DIR/wpa_supplicant/wpa_supplicant
-#$ROOT_DIR/../../../prebuilts/gcc/linux-x86/arm/arm-linux-harmonyeabi-gcc/bin/arm-linux-harmonyeabi-strip $ROOT_DIR/hostapd/hostapd
-#if [ ! -d $BIN_DIR ]; then
-#    mkdir -p $BIN_DIR
-#fi
-
-#cp $ROOT_DIR/wpa_supplicant/wpa_supplicant $BIN_DIR
-#cp $ROOT_DIR/hostapd/hostapd $BIN_DIR
+if [ "$2" = true ]; then
+cp $ROOT_DIR/wpa_supplicant/libwpa_client.so $OUT_DIR/ndk/sysroot/usr/lib
+cp $ROOT_DIR/build/libwpa.so $OUT_DIR/ndk/sysroot/usr/lib
+cp $ROOT_DIR/src/common/wpa_ctrl.h $OUT_DIR/ndk/sysroot/usr/include
+fi
