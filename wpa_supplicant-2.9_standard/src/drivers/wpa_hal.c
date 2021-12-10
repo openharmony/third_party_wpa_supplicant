08d00aa7645ee90c2e11ab436fd3b4b6247196ab
YOUR_NAME <fangdong7@huawei.com>
2021-12-10 08:16:01 +0000

    Description: wlan 5GHz adapter Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: YOUR_NAME <fangdong7@huawei.com>

--- wpa_supplicant-2.9_standard/src/drivers/wpa_hal.c
+++ wpa_supplicant-2.9_standard/src/drivers/wpa_hal.c
@@ -1093,7 +1093,7 @@ static struct hostapd_hw_modes *WifiWpaGetHwFeatureData(void *priv, uint16_t *nu
             if (index <= DEFAULT_NUM_MODES) {
                 modes[index].rates[loop] = hwFeatureData.bitrate[loop];
             } else {
-                modes[index].rates[loop] = hwFeatureData.bitrate[loop + 4];
+                modes[index].rates[loop] = hwFeatureData.bitrate[loop + IEEE80211B_RATES_NUM];
             }
         }
     }
