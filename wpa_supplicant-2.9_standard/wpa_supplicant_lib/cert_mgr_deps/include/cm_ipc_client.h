6e9a17cb3d3d4c19bdbb9fe0591191373581a489
Aurora <liuxiyao223@huawei.com>
2023-12-23 13:08:08 +0000

    update wpa_supplicant-2.9_standard/wpa_supplicant_lib/cert_mgr_deps/include/cm_ipc_client.h.
    Signed-off-by: Aurora <liuxiyao223@huawei.com>

--- wpa_supplicant-2.9_standard/wpa_supplicant_lib/cert_mgr_deps/include/cm_ipc_client.h
+++ wpa_supplicant-2.9_standard/wpa_supplicant_lib/cert_mgr_deps/include/cm_ipc_client.h
@@ -24,32 +24,7 @@ extern "C"
 {
 #endif
 
-int32_t CmClientGetCertList(const uint32_t store, struct CertList *certificateList);
-
-int32_t CmClientGetCertInfo(const struct CmBlob *certUri, const uint32_t store,
-    struct CertInfo *certificateInfo);
-
-int32_t CmClientSetCertStatus(const struct CmBlob *certUri, const uint32_t store,
-    const uint32_t status);
-
-int32_t CmClientInstallAppCert(const struct CmBlob *appCert, const struct CmBlob *appCertPwd,
-    const struct CmBlob *certAlias, const uint32_t store, struct CmBlob *keyUri);
-
-int32_t CmClientUninstallAppCert(const struct CmBlob *keyUri, const uint32_t store);
-
-int32_t CmClientUninstallAllAppCert(enum CertManagerInterfaceCode type);
-
-int32_t CmClientGetAppCertList(const uint32_t store, struct CredentialList *certificateList);
-
 int32_t CmClientGetAppCert(const struct CmBlob *keyUri, const uint32_t store, struct Credential *certificate);
-
-int32_t CmClientGrantAppCertificate(const struct CmBlob *keyUri, uint32_t appUid, struct CmBlob *authUri);
-
-int32_t CmClientGetAuthorizedAppList(const struct CmBlob *keyUri, struct CmAppUidList *appUidList);
-
-int32_t CmClientIsAuthorizedApp(const struct CmBlob *authUri);
-
-int32_t CmClientRemoveGrantedApp(const struct CmBlob *keyUri, uint32_t appUid);
 
 int32_t CmClientInit(const struct CmBlob *authUri, const struct CmSignatureSpec *spec, struct CmBlob *handle);
 
@@ -59,20 +34,6 @@ int32_t CmClientFinish(const struct CmBlob *handle, const struct CmBlob *inData,
 
 int32_t CmClientAbort(const struct CmBlob *handle);
 
-int32_t CmClientGetUserCertList(const uint32_t store, struct CertList *certificateList);
-
-int32_t CmClientGetUserCertInfo(const struct CmBlob *certUri, const uint32_t store,
-    struct CertInfo *certificateInfo);
-
-int32_t CmClientSetUserCertStatus(const struct CmBlob *certUri, const uint32_t store,
-    const uint32_t status);
-
-int32_t CmClientInstallUserTrustedCert(const struct CmBlob *userCert, const struct CmBlob *certAlias,
-    struct CmBlob *certUri);
-
-int32_t CmClientUninstallUserTrustedCert(const struct CmBlob *certUri);
-
-int32_t CmClientUninstallAllUserTrustedCert(void);
 
 #ifdef __cplusplus
 }
