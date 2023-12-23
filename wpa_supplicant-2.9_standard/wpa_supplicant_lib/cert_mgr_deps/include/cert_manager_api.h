04684639f586e0f40671314c4534cdb4027f11ea
Aurora <liuxiyao223@huawei.com>
2023-12-23 13:03:01 +0000

    update wpa_supplicant-2.9_standard/wpa_supplicant_lib/cert_mgr_deps/include/cert_manager_api.h.
    Signed-off-by: Aurora <liuxiyao223@huawei.com>

--- wpa_supplicant-2.9_standard/wpa_supplicant_lib/cert_mgr_deps/include/cert_manager_api.h
+++ wpa_supplicant-2.9_standard/wpa_supplicant_lib/cert_mgr_deps/include/cert_manager_api.h
@@ -22,32 +22,7 @@
 extern "C" {
 #endif
 
-CM_API_EXPORT int32_t CmGetCertList(uint32_t store, struct CertList *certificateList);
-
-CM_API_EXPORT int32_t CmGetCertInfo(const struct CmBlob *certUri, uint32_t store,
-    struct CertInfo *certificateInfo);
-
-CM_API_EXPORT int32_t CmSetCertStatus(const struct CmBlob *certUri, const uint32_t store,
-    const bool status);
-
-CM_API_EXPORT int32_t CmInstallAppCert(const struct CmBlob *appCert, const struct CmBlob *appCertPwd,
-    const struct CmBlob *certAlias, const uint32_t store, struct CmBlob *keyUri);
-
-CM_API_EXPORT int32_t CmUninstallAppCert(const struct CmBlob *keyUri, const uint32_t store);
-
-CM_API_EXPORT int32_t CmUninstallAllAppCert(void);
-
-CM_API_EXPORT int32_t CmGetAppCertList(const uint32_t store, struct CredentialList *certificateList);
-
 CM_API_EXPORT int32_t CmGetAppCert(const struct CmBlob *keyUri, const uint32_t store, struct Credential *certificate);
-
-CM_API_EXPORT int32_t CmGrantAppCertificate(const struct CmBlob *keyUri, uint32_t appUid, struct CmBlob *authUri);
-
-CM_API_EXPORT int32_t CmGetAuthorizedAppList(const struct CmBlob *keyUri, struct CmAppUidList *appUidList);
-
-CM_API_EXPORT int32_t CmIsAuthorizedApp(const struct CmBlob *authUri);
-
-CM_API_EXPORT int32_t CmRemoveGrantedApp(const struct CmBlob *keyUri, uint32_t appUid);
 
 CM_API_EXPORT int32_t CmInit(const struct CmBlob *authUri, const struct CmSignatureSpec *spec, struct CmBlob *handle);
 
@@ -56,21 +31,6 @@ CM_API_EXPORT int32_t CmUpdate(const struct CmBlob *handle, const struct CmBlob 
 CM_API_EXPORT int32_t CmFinish(const struct CmBlob *handle, const struct CmBlob *inData, struct CmBlob *outData);
 
 CM_API_EXPORT int32_t CmAbort(const struct CmBlob *handle);
-
-CM_API_EXPORT int32_t CmGetUserCertList(uint32_t store, struct CertList *certificateList);
-
-CM_API_EXPORT int32_t CmGetUserCertInfo(const struct CmBlob *certUri, uint32_t store,
-    struct CertInfo *certificateInfo);
-
-CM_API_EXPORT int32_t CmSetUserCertStatus(const struct CmBlob *certUri, uint32_t store,
-    const bool status);
-
-CM_API_EXPORT int32_t CmInstallUserTrustedCert(const struct CmBlob *userCert,
-    const struct CmBlob *certAlias, struct CmBlob *certUri);
-
-CM_API_EXPORT int32_t CmUninstallUserTrustedCert(const struct CmBlob *certUri);
-
-CM_API_EXPORT int32_t CmUninstallAllUserTrustedCert(void);
 
 #ifdef __cplusplus
 }
