38c56b1122eed21e6350d00e9da5fdb0f6649a07
xionglei <xionglei3@huawei.com>
2024-07-15 08:37:16 +0000

    1
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wpa_supplicant-2.9_standard/wpa_supplicant/bss.c
+++ wpa_supplicant-2.9_standard/wpa_supplicant/bss.c
@@ -228,7 +228,11 @@ void wpa_bss_remove(struct wpa_supplicant *wpa_s, struct wpa_bss *bss,
 	dl_list_del(&bss->list);
 	dl_list_del(&bss->list_id);
 	wpa_s->num_bss--;
-	wpa_msg_only_for_cb(wpa_s, MSG_DEBUG, "BSS: Remove id %u BSSID " MACSTR
+	int level = MSG_DEBUG;
+#ifdef HW_WPA_REDUCE_LOG
+	level = MSG_EXCESSIVE;
+#endif /* HW_WPA_REDUCE_LOG */
+	wpa_msg_only_for_cb(wpa_s, level, "BSS: Remove id %u BSSID " MACSTR
 		" SSID '%s' due to %s", bss->id, MAC2STR(bss->bssid),
 		wpa_ssid_txt(bss->ssid, bss->ssid_len), reason);
 	wpa_printf(MSG_INFO, "BSS: Remove id %u BSSID " MACSTR_SEC
@@ -492,7 +496,11 @@ static struct wpa_bss * wpa_bss_add(struct wpa_supplicant *wpa_s,
 			    MAC2STR(bss->hessid));
 	else
 		extra[0] = '\0';
-	wpa_msg_only_for_cb(wpa_s, MSG_DEBUG, "BSS: Add new id %u BSSID " MACSTR
+	int level = MSG_DEBUG;
+#ifdef HW_WPA_REDUCE_LOG
+	level = MSG_EXCESSIVE;
+#endif /* HW_WPA_REDUCE_LOG */
+	wpa_msg_only_for_cb(wpa_s, level, "BSS: Add new id %u BSSID " MACSTR
 		" SSID '%s' freq %d%s",
 		bss->id, MAC2STR(bss->bssid), wpa_ssid_txt(ssid, ssid_len),
 		bss->freq, extra);
@@ -777,8 +785,13 @@ wpa_bss_update(struct wpa_supplicant *wpa_s, struct wpa_bss *bss,
 void wpa_bss_update_start(struct wpa_supplicant *wpa_s)
 {
 	wpa_s->bss_update_idx++;
+#ifdef HW_WPA_REDUCE_LOG
+	wpa_dbg(wpa_s, MSG_EXCESSIVE, "BSS: Start scan result update %u",
+		wpa_s->bss_update_idx);
+#else
 	wpa_dbg(wpa_s, MSG_INFO, "BSS: Start scan result update %u",
 		wpa_s->bss_update_idx);
+#endif /* HW_WPA_REDUCE_LOG */
 	wpa_s->last_scan_res_used = 0;
 }
 
@@ -855,8 +868,13 @@ void wpa_bss_update_scan_res(struct wpa_supplicant *wpa_s,
 		 * If it's a P2P specific interface, then don't update
 		 * the scan result without a P2P IE.
 		 */
+#ifdef HW_WPA_REDUCE_LOG
+		wpa_printf(MSG_EXCESSIVE, "BSS: No P2P IE - skipping BSS " MACSTR_SEC
+			   " update for P2P interface", MAC2STR_SEC(res->bssid));
+#else
 		wpa_printf(MSG_DEBUG, "BSS: No P2P IE - skipping BSS " MACSTR_SEC
 			   " update for P2P interface", MAC2STR_SEC(res->bssid));
+#endif /* HW_WPA_REDUCE_LOG */
 		return;
 #ifdef CONFIG_MAGICLINK_PC
 		}
