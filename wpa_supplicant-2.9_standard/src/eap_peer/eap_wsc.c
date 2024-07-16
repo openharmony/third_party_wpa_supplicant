5f0867089da63d009b026d9eca76ecaaa2439f08
xionglei <xionglei3@huawei.com>
2024-07-16 09:37:05 +0000

    1
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wpa_supplicant-2.9_standard/src/eap_peer/eap_wsc.c
+++ wpa_supplicant-2.9_standard/src/eap_peer/eap_wsc.c
@@ -48,7 +48,7 @@ static const char * eap_wsc_state_txt(int state)
 
 static void eap_wsc_state(struct eap_wsc_data *data, int state)
 {
-#ifdef HW_WPA_REDUCE_LOG
+#ifdef WPA_REDUCE_LOG
 	wpa_printf(MSG_EXCESSIVE, "EAP-WSC: %s -> %s",
 		   eap_wsc_state_txt(data->state),
 		   eap_wsc_state_txt(state));
@@ -311,11 +311,11 @@ static struct wpabuf * eap_wsc_build_msg(struct eap_wsc_data *data,
 	size_t send_len, plen;
 
 	ret->ignore = false;
-#ifdef HW_WPA_REDUCE_LOG
+#ifdef WPA_REDUCE_LOG
 	wpa_printf(MSG_EXCESSIVE, "EAP-WSC: Generating Response");
 #else
 	wpa_printf(MSG_DEBUG, "EAP-WSC: Generating Response");
-#endif /* HW_WPA_REDUCE_LOG */
+#endif /* WPA_REDUCE_LOG */
 	ret->allowNotifications = true;
 
 	flags = 0;
@@ -349,7 +349,7 @@ static struct wpabuf * eap_wsc_build_msg(struct eap_wsc_data *data,
 	ret->decision = DECISION_FAIL;
 
 	if (data->out_used == wpabuf_len(data->out_buf)) {
-#ifdef HW_WPA_REDUCE_LOG
+#ifdef WPA_REDUCE_LOG
 		wpa_printf(MSG_EXCESSIVE, "EAP-WSC: Sending out %lu bytes "
 			   "(message sent completely)",
 			   (unsigned long) send_len);
@@ -357,7 +357,7 @@ static struct wpabuf * eap_wsc_build_msg(struct eap_wsc_data *data,
 		wpa_printf(MSG_DEBUG, "EAP-WSC: Sending out %lu bytes "
 			   "(message sent completely)",
 			   (unsigned long) send_len);
-#endif /* HW_WPA_REDUCE_LOG */
+#endif /* WPA_REDUCE_LOG */
 		wpabuf_free(data->out_buf);
 		data->out_buf = NULL;
 		data->out_used = 0;
