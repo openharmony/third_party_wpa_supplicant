3d5a609d15528efa8df62164ad73680200a00f51
xionglei <xionglei3@huawei.com>
2024-07-30 09:44:58 +0000

    1
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wpa_supplicant-2.9_standard/src/ap/sta_info.c
+++ wpa_supplicant-2.9_standard/src/ap/sta_info.c
@@ -300,13 +300,8 @@ void ap_free_sta(struct hostapd_data *hapd, struct sta_info *sta)
 
 	if (set_beacon)
 		ieee802_11_set_beacons(hapd->iface);
-#ifdef WPA_REDUCE_LOG
 	wpa_printf(MSG_EXCESSIVE, "%s: cancel ap_handle_timer for " MACSTR_SEC,
 		   __func__, MAC2STR_SEC(sta->addr));
-#else
-	wpa_printf(MSG_DEBUG, "%s: cancel ap_handle_timer for " MACSTR_SEC,
-		   __func__, MAC2STR_SEC(sta->addr));
-#endif /* WPA_REDUCE_LOG */
 	eloop_cancel_timeout(ap_handle_timer, hapd, sta);
 	eloop_cancel_timeout(ap_handle_session_timer, hapd, sta);
 	eloop_cancel_timeout(ap_handle_session_warning_timer, hapd, sta);
@@ -762,11 +757,7 @@ struct sta_info * ap_sta_add(struct hostapd_data *hapd, const u8 *addr)
 	sta->supported_rates_len = i;
 
 	if (!(hapd->iface->drv_flags & WPA_DRIVER_FLAGS_INACTIVITY_TIMER)) {
-		int level = MSG_DEBUG;
-#ifdef WPA_REDUCE_LOG
-		level = MSG_EXCESSIVE;
-#endif /* WPA_REDUCE_LOG */
-		wpa_printf(level, "%s: register ap_handle_timer timeout "
+		wpa_printf(MSG_EXCESSIVE, "%s: register ap_handle_timer timeout "
 			   "for " MACSTR_SEC " (%d seconds - ap_max_inactivity)",
 			   __func__, MAC2STR_SEC(addr),
 			   hapd->conf->ap_max_inactivity);
@@ -1525,11 +1516,7 @@ void ap_sta_disconnect(struct hostapd_data *hapd, struct sta_info *sta,
 	hostapd_set_sta_flags(hapd, sta);
 	wpa_auth_sm_event(sta->wpa_sm, WPA_DEAUTH);
 	ieee802_1x_notify_port_enabled(sta->eapol_sm, 0);
-	int level = MSG_DEBUG;
-#ifdef WPA_REDUCE_LOG
-	level = MSG_EXCESSIVE;
-#endif /* WPA_REDUCE_LOG */
-	wpa_printf(level, "%s: %s: reschedule ap_handle_timer timeout "
+	wpa_printf(MSG_EXCESSIVE, "%s: %s: reschedule ap_handle_timer timeout "
 		   "for " MACSTR_SEC " (%d seconds - "
 		   "AP_MAX_INACTIVITY_AFTER_DEAUTH)",
 		   hapd->conf->iface, __func__, MAC2STR_SEC(sta->addr),
@@ -1601,11 +1588,7 @@ void ap_sta_clear_disconnect_timeouts(struct hostapd_data *hapd,
 			   hapd->conf->iface, MAC2STR_SEC(sta->addr));
 	if (eloop_cancel_timeout(ap_sta_delayed_1x_auth_fail_cb, hapd, sta) > 0)
 	{
-		int level = MSG_DEBUG;
-#ifdef WPA_REDUCE_LOG
-		level = MSG_EXCESSIVE;
-#endif /* WPA_REDUCE_LOG */
-		wpa_printf(level,
+		wpa_printf(MSG_EXCESSIVE,
 			   "%s: Removed ap_sta_delayed_1x_auth_fail_cb timeout for "
 			   MACSTR_SEC,
 			   hapd->conf->iface, MAC2STR_SEC(sta->addr));
