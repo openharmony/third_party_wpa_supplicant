563ce483055f0e85a24a250d99aaee6c4651f326
xionglei <xionglei3@huawei.com>
2024-07-30 11:18:52 +0000

    1
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wpa_supplicant-2.9_standard/src/eap_server/eap_server_wsc.c
+++ wpa_supplicant-2.9_standard/src/eap_server/eap_server_wsc.c
@@ -54,15 +54,9 @@ static const char * eap_wsc_state_txt(int state)
 
 static void eap_wsc_state(struct eap_wsc_data *data, int state)
 {
-#ifdef WPA_REDUCE_LOG
 	wpa_printf(MSG_EXCESSIVE, "EAP-WSC: %s -> %s",
 		   eap_wsc_state_txt(data->state),
 		   eap_wsc_state_txt(state));
-#else
-	wpa_printf(MSG_DEBUG, "EAP-WSC: %s -> %s",
-		   eap_wsc_state_txt(data->state),
-		   eap_wsc_state_txt(state));
-#endif
 	data->state = state;
 }
 
@@ -228,15 +222,9 @@ static struct wpabuf * eap_wsc_build_msg(struct eap_wsc_data *data, u8 id)
 	data->out_used += send_len;
 
 	if (data->out_used == wpabuf_len(data->out_buf)) {
-#ifdef WPA_REDUCE_LOG
 		wpa_printf(MSG_EXCESSIVE, "EAP-WSC: Sending out %lu bytes "
 			   "(message sent completely)",
 			   (unsigned long) send_len);
-#else
-		wpa_printf(MSG_DEBUG, "EAP-WSC: Sending out %lu bytes "
-			   "(message sent completely)",
-			   (unsigned long) send_len);
-#endif /* WPA_REDUCE_LOG */
 		wpabuf_free(data->out_buf);
 		data->out_buf = NULL;
 		data->out_used = 0;
