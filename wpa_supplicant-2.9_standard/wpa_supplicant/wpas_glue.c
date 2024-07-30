37701c4827eabc88324179791c43ccfee05f471f
xionglei <xionglei3@huawei.com>
2024-07-30 13:01:56 +0000

    1
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wpa_supplicant-2.9_standard/wpa_supplicant/wpas_glue.c
+++ wpa_supplicant-2.9_standard/wpa_supplicant/wpas_glue.c
@@ -1070,13 +1070,8 @@ static void wpa_supplicant_port_cb(void *ctx, int authorized)
 		return;
 	}
 #endif /* CONFIG_AP */
-#ifdef WPA_REDUCE_LOG
 	wpa_printf(MSG_EXCESSIVE, "EAPOL: Supplicant port status: %s",
 		   authorized ? "Authorized" : "Unauthorized");
-#else
-	wpa_printf(MSG_DEBUG, "EAPOL: Supplicant port status: %s",
-		   authorized ? "Authorized" : "Unauthorized");
-#endif /* WPA_REDUCE_LOG */
 	wpa_drv_set_supp_port(wpa_s, authorized);
 }
 
