0b0a624d0c37986d7a228856c04a76a4e64cf4d1
xionglei <xionglei3@huawei.com>
2024-07-30 10:00:48 +0000

    1
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wpa_supplicant-2.9_standard/src/drivers/driver_nl80211_capa.c
+++ wpa_supplicant-2.9_standard/src/drivers/driver_nl80211_capa.c
@@ -2409,7 +2409,6 @@ static int nl80211_set_regulatory_flags(struct wpa_driver_nl80211_data *drv,
 				  NULL, NULL);
 }
 
-#ifndef WPA_REDUCE_LOG
 static const char * modestr(enum hostapd_hw_mode mode)
 {
 	switch (mode) {
@@ -2425,7 +2424,6 @@ static const char * modestr(enum hostapd_hw_mode mode)
 		return "?";
 	}
 }
-#endif
 
 static void nl80211_dump_chan_list(struct hostapd_hw_modes *modes,
 				   u16 num_modes)
