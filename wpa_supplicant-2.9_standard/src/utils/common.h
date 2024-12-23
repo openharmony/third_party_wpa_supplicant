27da7dc264256cf404c52a756d5eb5fff92a748f
xiaosi <xiaosi2@huawei.com>
2024-12-23 17:50:03 +0800

    AI检测异常处理
    Signed-off-by: xiaosi <xiaosi2@huawei.com>

--- wpa_supplicant-2.9_standard/src/utils/common.h
+++ wpa_supplicant-2.9_standard/src/utils/common.h
@@ -24,6 +24,9 @@
 #define CHANNEL_157 157
 #define CHANNEL_161 161
 
+#define NUMBER_BASE 10
+#define MAX_INT32_LENGTH 11
+
 #if defined(__linux__) || defined(__GLIBC__)
 #include <endian.h>
 #include <byteswap.h>
@@ -612,6 +615,8 @@ char * get_param(const char *cmd, const char *param);
 void forced_memzero(void *ptr, size_t len);
 
 const char *mac_to_str(const u8 *addr);
+
+unsigned int AtoiToStrtol(const char *input);
 
 /*
  * gcc 4.4 ends up generating strict-aliasing warnings about some very common
