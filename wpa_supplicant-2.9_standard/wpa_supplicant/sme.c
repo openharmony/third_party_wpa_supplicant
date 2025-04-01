ae2b520f93f78cdee8704ba2d81a8186f7e72e35
吴俊 <wujun156@huawei.com>
2025-04-01 15:28:35 +0800

    add
    Signed-off-by: 吴俊 <wujun156@huawei.com>

--- wpa_supplicant-2.9_standard/wpa_supplicant/sme.c
+++ wpa_supplicant-2.9_standard/wpa_supplicant/sme.c
@@ -1408,11 +1408,12 @@ static int sme_external_auth_send_sae_commit(struct wpa_supplicant *wpa_s,
 		status = WLAN_STATUS_SUCCESS;
 	sme_external_auth_build_buf(buf, resp, wpa_s->own_addr,
 #ifdef CONFIG_MLD_PATCH
-					bssid, 1,
+					bssid,
 #else
 				    wpa_s->sme.ext_ml_auth ?
-				    wpa_s->sme.ext_auth_ap_mld_addr : bssid, 1,
+				    wpa_s->sme.ext_auth_ap_mld_addr : bssid,
 #endif
+					1,
 				    wpa_s->sme.seq_num, status,
 				    wpa_s->sme.ext_ml_auth ?
 				    wpa_s->own_addr : NULL);
