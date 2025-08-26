aaa5f100e831daf8633bcc558d2882d9db44fc67
Simpleytz <zengyating@huawei.com>
2025-08-26 19:01:14 +0800

    add a timeout mechanism
    Signed-off-by: Simpleytz <zengyating@huawei.com>

--- wpa_supplicant-2.9_standard/wpa_supplicant_lib/cert_mgr_deps/src/cm_request.cpp
+++ wpa_supplicant-2.9_standard/wpa_supplicant_lib/cert_mgr_deps/src/cm_request.cpp
@@ -47,7 +47,7 @@ static sptr<IRemoteObject> CmLoadSystemAbility(void)
         return object;
     }
 
-    return  saManager->LoadSystemAbility(SA_ID_KEYSTORE_SERVICE,LOAD_ABILITY_TIME_OUT_SECONDS); 
+    return saManager->LoadSystemAbility(SA_ID_KEYSTORE_SERVICE, LOAD_ABILITY_TIME_OUT_SECONDS); 
 }
 
 static int32_t CmReadRequestReply(MessageParcel &reply, struct CmBlob *outBlob)
