5a92ac27d4785401105b4265ee55f77014cb5524
oh_ci <maguangsheng1@huawei.com>
2025-09-06 08:05:05 +0000

    回退 'Pull Request !702 : libnl3.11'

--- wpa_supplicant-2.9_standard/src/utils/common.h
+++ wpa_supplicant-2.9_standard/src/utils/common.h
@@ -606,9 +606,7 @@ void int_array_sort_unique(int *a);
 void int_array_add_unique(int **res, int a);
 bool int_array_includes(int *arr, int val);
 
-#ifndef ARRAY_SIZE
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
-#endif
 
 void str_clear_free(char *str);
 void bin_clear_free(void *bin, size_t len);
