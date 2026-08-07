f219a329e48277af6d2a19a172285ab76d4d22db
temptest_01 <15068825070@163.com>
2026-08-07 10:17:05 +0800

    软总线xts修复
    Signed-off-by: temptest_01 <15068825070@163.com>

--- wpa_supplicant-2.9_standard/wpa_supplicant/events.c
+++ wpa_supplicant-2.9_standard/wpa_supplicant/events.c
@@ -6529,6 +6529,30 @@ void set_running_wpa()
 	run_mode = 0;
 }
 
+#ifdef CONFIG_OPEN_HARMONY_PATCH
+static void restart_p2p_device(struct wpa_supplicant *wpa_s)
+{
+	struct wpa_global *global = wpa_s->global;
+	struct wpa_supplicant *iface;
+
+	if (strncmp(wpa_s->ifname, "wlan0", strlen("wlan0")) != 0) {
+		return;
+	}
+	if (global == NULL) {
+		return;
+	}
+	iface = global->ifaces;
+	while(iface) {
+		if (strncmp(iface->ifname, "p2p-dev-wlan0", strlen("p2p-dev-wlan0")) == 0) {
+			wpa_supplicant_remove_iface(global, iface, 1);
+			wpas_p2p_add_p2pdev_interface(wpa_s, wpa_s->global->params.conf_p2p_dev);
+			break;
+		}
+		iface = iface->next;
+	}
+}
+#endif /* CONFIG_OPEN_HARMONY_PATCH */
+
 void wpa_supplicant_event_hapd(void *ctx, enum wpa_event_type event, union wpa_event_data *data);
 
 void wpa_supplicant_event(void *ctx, enum wpa_event_type event,
@@ -7208,6 +7232,9 @@ void wpa_supplicant_event(void *ctx, enum wpa_event_type event,
 	case EVENT_INTERFACE_MAC_CHANGED:
 		wpa_supplicant_update_mac_addr(wpa_s);
 		wpa_sm_pmksa_cache_flush(wpa_s->wpa, NULL);
+#ifdef CONFIG_OPEN_HARMONY_PATCH
+		restart_p2p_device(wpa_s);
+#endif /* CONFIG_OPEN_HARMONY_PATCH */
 		break;
 	case EVENT_INTERFACE_ENABLED:
 		wpa_dbg(wpa_s, MSG_DEBUG, "Interface was enabled");
@@ -7243,6 +7270,9 @@ void wpa_supplicant_event(void *ctx, enum wpa_event_type event,
 			wpa_supplicant_req_scan(wpa_s, 0, 0);
 #endif /* CONFIG_AP */
 		}
+#ifdef CONFIG_OPEN_HARMONY_PATCH
+		restart_p2p_device(wpa_s);
+#endif /* CONFIG_OPEN_HARMONY_PATCH */
 		break;
 	case EVENT_INTERFACE_DISABLED:
 		wpa_dbg(wpa_s, MSG_DEBUG, "Interface was disabled");
